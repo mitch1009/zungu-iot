868debc0f36ba4d377cbdfb62dfb3fcbdf1cc853
Mitch Chimwemwe Chanza <66210356+mitch1009@users.noreply.github.com>
2021-10-21 22:24:45 +0200

    update

--- main/controller/__init__.py
+++ main/controller/__init__.py
@@ -33,7 +33,7 @@ def github_repository_post_hook():
     pprint(request.json)
     response = request.json
     aut = "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"
-    auth = f"https://{aut}:x-oauth-basic@github.com@github.com/mitch1009/php-boilerplate.git"
+    auth = f"https://{aut}:x-oauth-basic@github.com/mitch1009/php-boilerplate.git"
     if os.path.exists(f"./repositories/php-boilerplate"):
         pull = git.cmd.Git(auth)
         pull.pull()
